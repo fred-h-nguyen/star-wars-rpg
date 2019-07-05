2f2f0b3aea0ad86e6f2eea18a6518349b5af9f22
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-07-05 15:34:06 -0500

    pseudocoded the game

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+// document ready
+//set var for hp
+//set var for atks
+//hide all images in certain divs
+//maybe use an array?
+//or an object
+// hide all messages
+//on click of first image hide all images and only reveal image clicked under your character the rest under enemies available to attack
+//on click remove character click and reveal character under defender
+//on click of the attack button
+//reveal message you have attackd for x damage
+//reveal you have taken y damage
+// change hp totals
+// if won choose new defender
+//repeat until all enemies are killed
+// restart butt shows up if your hp is 0 or all enemies killed
+// you win no enemies left you lose if ur hp <=0
